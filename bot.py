d2fe9cc7350f345eaf6dd25f4fc4db19d28bd258
Ilya Malyshev <55033489+malyshevig@users.noreply.github.com>
2023-09-01 09:01:14 +0300

    add to git

---
+++ bot.py
@@ -0,0 +1,102 @@
+import telebot, logging
+from typing import Optional
+from telebot.types import Document,Message
+from loader import Download, Task
+import sys
+import threading, queue
+
+
+
+
+
+user = "397071083"
+name = "iml"
+token = "6610941656:AAHrCB_BSZsxnV-rouK8Zh0X4e3LSG8-FOo"
+logging.basicConfig(stream=sys.stdout,encoding='utf-8', format='%(asctime)s %(levelname)s %(message)s', level=logging.INFO)
+
+
+class Bot:
+
+    def __init__(self, q:queue.Queue):
+        self.task_num = 1
+        self.q = q
+        self.token = token
+
+        self.bot = telebot.TeleBot(f'{self.token}')
+        text_handle = lambda msg: self.get_text_message(msg)
+        doc_handle = lambda msg: self.get_doc(msg)
+
+        self.bot.message_handler(content_types=['text'])(text_handle)
+        self.bot.message_handler(content_types=['document'])(doc_handle)
+
+
+    def run(self):
+        logging.info("Bot started polling")
+        self.bot.polling(none_stop=True, interval=0)
+
+    def get_text_message(self, message: Optional[Message]):
+        try:
+            logging.info(f" {message.from_user} recieved = {message}")
+            #s = Download(message.text)
+            t = self.task_num
+            self.task_num = self.task_num+1
+
+            link = message.text
+            self.q.put(Task(id=f"task {t}", link=link, msg= message))
+
+            logging.info(f" queue: {q.qsize()}")
+#            if message.text.endswith(".torrent"):
+#                logging.info(f" {message.from_user} recieved torrent URL = {message.text}")
+#                self.hand.process_url(message.text)
+
+            self.bot.send_message(message.from_user.id, f"added task {t} {link}")
+
+        except Exception as err:
+            logging.error(err)
+
+    def get_doc(self,message: Optional[Document]):
+        try:
+            if not self.hand.auth(message.from_user.id):
+                return
+            logging.info(f" {message.from_user} recieved file {message.document}")
+
+            file_name: str = message.document.file_name
+            if not file_name.endswith(".torrent"):
+                logging.info(f" {message.from_user} unknown type of file {message.document}")
+                self.bot.send_message(message.from_user.id, f"Dont know file type of {file_name}")
+
+            self.bot.send_message(message.from_user.id, "Processing started")
+
+        except Exception as err:
+            logging.error(err)
+
+
+class Downloader (threading.Thread):
+    def __init__(self, q:queue.Queue, bot: Bot):
+        super().__init__()
+        self.q = q
+        self.bot = bot
+
+    def run(self):
+        while True:
+            try:
+                logging.info(f"queue {self.q.qsize()}")
+                t:Task =  self.q.get()
+                msg:Message = t.msg
+                logging.info(f"downloading {msg.text}")
+                s = Download(msg.text)
+                logging.info(f"downloading res {s}")
+                self.bot.bot.send_message(msg.from_user.id, f"{t.id} {s} {msg.text}")
+                logging.info(f"result task:{t} {s} {msg.text}")
+            except BaseException as ex:
+                logging.error(f"downloading error: {ex}")
+
+
+if __name__ == "__main__":
+    q = queue.Queue()
+
+    bot = Bot(q)
+    Downloader(q, bot).start()
+    Downloader(q, bot).start()
+    Downloader(q, bot).start()
+    bot.run()
---
+++ loader.py
@@ -0,0 +1,63 @@
+from pytube import YouTube
+from pytube import Playlist
+import os, sys
+import logging
+
+logging.basicConfig(stream=sys.stdout,encoding='utf-8', format='%(asctime)s %(levelname)s %(message)s', level=logging.INFO)
+
+url= "https://www.youtube.com/watch?v=HytWgel76rY&list=PLEwK9wdS5g0oZwFwoQT-BrjmkazJWXxfe"
+dir= "/Volumes/SSD/video/youtube"
+
+
+class Task:
+    def __init__(self, id, link, msg, target=dir):
+        self.id = id
+        self.link = link
+        self.target = target
+        self.msg = msg
+
+
+def is_playlist(link)-> bool:
+    return link is not None and link.find("playlist?") >=0
+
+
+def download_playlist(link) -> str:
+    playlist = Playlist(link)
+    logging.info(f"download playlist {playlist.title}")
+
+    new_dir = dir+"/"+playlist.title
+    if not os.path.exists(new_dir):
+        os.mkdir(new_dir)
+
+    for video in playlist.videos:
+        print('downloading : {} with url : {}'.format(video.title, video.watch_url))
+        download_video(v=video,dest_dir=new_dir)
+
+    return "ok"
+
+
+def download_video(v, dest_dir=dir) -> str:
+    v = v.streams.get_highest_resolution()
+    try:
+        v.download(output_path=dest_dir)
+    except BaseException as ex:
+        print(f"An error has occurred {ex}")
+        return "An error has occurred"
+
+    r = "Download is completed successfully"
+    return r
+
+
+def download_link(link)->str:
+    youtube_object = YouTube(url=link)
+    return download_video(youtube_object)
+
+
+def Download(link)->str:
+    if is_playlist(link):
+        return download_playlist(link)
+    else:
+        return download_link(link)
+
+
+
